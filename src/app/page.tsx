47dfde9aa96571d0e7cc8b06e350e1cfb50c3c9e
gustcomer <gustcomer@gmail.com>
2024-07-07 23:51:43 -0300

    feat: add links to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 "use client"
-
-import SplitScreen from "../components/split-screen-sc"
-
-const LeftSideComp = () => {
-  return <h2>I am left!</h2>
-};
-
-const RightSideComp = () => {
-  return <h2>I am right!</h2>
-};
+import Link from "next/link";
 
 export default function Home() {
-  return <SplitScreen Left={LeftSideComp} Right={RightSideComp} />;
+  return(
+    <>
+      <h2>Design Patterns</h2>
+      <p><Link href='/experiments/001-split-screen-sc'>Split Screen using Styled Components</Link></p>
+      <p><Link href='/experiments/002-split-screen-tw'>Split Screen using Tailwind</Link></p>
+      <p><Link href='/experiments/003-split-screen-enhanced-sc'>Enhanced Split Screen using Styled Components</Link></p>
+      <p><Link href='/experiments/004-split-screen-enhanced-tw'>Enhanced Split Screen using Tailwind</Link></p>
+      <p><Link href='/experiments/005-split-screen-enhanced-sc-children'>Enhanced Split Screen using Styled Components passing subcomponents as children insted of props</Link></p>
+      <p><Link href='/experiments/006-list'>List</Link></p>
+      <p><Link href='/experiments/007-modal'>Modal</Link></p>
+    </>
+  );
 }
